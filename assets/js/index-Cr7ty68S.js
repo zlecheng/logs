84689d7c576fe062b60a569f68250abcb69bb418
zlecheng <z_hongpan@163.com>
2025-02-18 20:59:10 +0800

    fix:修改

---
+++ assets/js/index-Cr7ty68S.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{Y as o,Z as t,_ as c}from"./index-eW_PD8YJ.js";const r="/logs/assets/png/welcome-VFYC1WKN.png",a={},n={class:"home card"};function _(l,e){return t(),o("div",n,e[0]||(e[0]=[c("img",{class:"home-bg",src:r,alt:"welcome"},null,-1)]))}const i=s(a,[["render",_],["__scopeId","data-v-70338efb"]]);export{i as default};
